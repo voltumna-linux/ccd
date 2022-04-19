fe4bba3a3101f7e051b1c8501a999a508a866f84
Alessio Igor Bogani <alessio.bogani@elettra.eu>
2022-04-19 18:57:12 +0200

    Make rnm-dpdk-conf service works

--- recipes-support/rnm-dpdk/files/rnm-dpdk-conf.sh
+++ recipes-support/rnm-dpdk/files/rnm-dpdk-conf.sh
@@ -3,13 +3,10 @@
 mkdir -p /run/rnm-dpdk
 touch /run/rnm-dpdk/rnm.conf
 
-NAME=$(hostnamectl --transient)-rnm
+NAME=$(hostname)-rnm
 DNSREPLY=$(resolvectl query $NAME)
 if [ "$?" -eq 0 ]; then
 	ADDRESS=$(echo "$DNSREPLY" | sed -n "s,$NAME: \([0-9.]*\) .*,\1,p")
 	LASTOCTECT=$(echo $ADDRESS | cut -d. -f4)
 	echo CLIENTID="-- --client-id $LASTOCTECT" >> /run/rnm-dpdk/rnm.conf
 fi
-
-
-
